35ce9d6ce0a68592b8a80901aa62e2008b16954b
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-15 09:40:38 +0000

    add 'addToCart reducer function

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,25 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 type CartItem = {
-    id: string;
-    title: string;
-    price: number;
-    quantity: number;
+	id: string;
+	title: string;
+	price: number;
+	quantity: number;
 };
 
 type CartState = {
-    items: CartItem[]
-}
+	items: CartItem[];
+};
 
 const initialState: CartState = {
-    items: []
-}
+	items: [],
+};
 
 export const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addToCart() {},
-        removeFromCart() {}
-    }
-})
+	name: 'cart',
+	initialState,
+	reducers: {
+		addToCart(
+			state,
+			action: PayloadAction<{ id: string; title: string; price: number }>
+		) {
+			// we get -1 if item is not found
+			const itemIndex = state.items.findIndex(
+				(item) => item.id === action.payload.id
+			);
+
+            if (itemIndex >= 0) {
+                state.items[itemIndex].quantity++
+            } else {
+                state.items.push({...action.payload, quantity: 1})
+            }
+		},
+		removeFromCart() {
+            
+        },
+	},
+});
