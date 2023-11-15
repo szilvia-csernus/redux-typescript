1434dc082e2f750f4559ade273fa32a900f6d0c9
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-15 09:42:30 +0000

    add 'removeFromCart' reducer function

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -34,8 +34,17 @@ export const cartSlice = createSlice({
                 state.items.push({...action.payload, quantity: 1})
             }
 		},
-		removeFromCart() {
-            
+		removeFromCart(state, action: PayloadAction<string>) {
+            const itemIndex = state.items.findIndex(
+                // this time, the whole payload is the id!
+                    (item) => item.id === action.payload
+                );
+    
+                if (state.items[itemIndex].quantity === 1) {
+                    state.items.splice(itemIndex, 1)
+                } else {
+                    state.items[itemIndex].quantity--;
+                }
         },
 	},
 });
